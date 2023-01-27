cfd586348897d775bdb2c764b2deff21eb50c2db
nofariof12 <111131511+nofariof12@users.noreply.github.com>
2023-01-27 16:40:05 +0200

    add pred files

--- Prophet.py
+++ Prophet.py
@@ -1,7 +1,17 @@
-import pandas
+import numpy as np
+import pandas as pd
 from prophet import Prophet
 
-df = pandas.read_csv("foreign exchange DATA\ILS\ILS_GBP.csv")
-pandas.set_option('display.max_rows', None)
+df = pd.read_csv(r'Finall Project\foreign exchange DATA\USD\USD_ILS.csv')
+pd.set_option('display.max_rows', None)
 
-print(df)
+m = Prophet()
+m.fit(df)
+future = m.make_future_dataframe(periods=365)
+future.tail()
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+x=forecast[forecast['ds'] >= pd.to_datetime('2023-01-01')]
+pd.DataFrame(x, columns=['ds','yhat']).to_csv('USD_ILS_pred.csv')
+
+print(x)
