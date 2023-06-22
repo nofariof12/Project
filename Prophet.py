2519254a51ed60427edbbfda2223d378b62cda30
nofariof12 <111131511+nofariof12@users.noreply.github.com>
2023-06-22 13:05:36 +0300

    save

--- Prophet.py
+++ Prophet.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pandas as pd
 from prophet import Prophet
 
-df = pd.read_csv(r'Finall Project\foreign exchange DATA\USD\USD_ILS.csv')
+df = pd.read_csv(r'Finall Project\foreign exchange DATA\EUR\EUR_GBP.csv')
 pd.set_option('display.max_rows', None)
 
 m = Prophet()
@@ -12,6 +11,6 @@ future.tail()
 forecast = m.predict(future)
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 x=forecast[forecast['ds'] >= pd.to_datetime('2023-01-01')]
-pd.DataFrame(x, columns=['ds','yhat']).to_csv('USD_ILS_pred.csv')
+pd.DataFrame(x, columns=['ds','yhat']).to_csv('XXX.csv')
 
 print(x)
