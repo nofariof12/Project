e63d097133016ce3d5ce8bc8c31274ff54ed7178
nofariof12 <111131511+nofariof12@users.noreply.github.com>
2023-08-24 02:46:22 +0300

    logoCoins+historyTable

--- react-site/src/components/Menu.js
+++ react-site/src/components/Menu.js
@@ -8,13 +8,16 @@ function Menu({ setActiveMenu }) {
   const setActiveMenuCalendar = () => {
       setActiveMenu("Historical_and_current_rates");
   }
+  const setActiveMenuHistory = () => {
+    setActiveMenu("My_history");
+}
   
   return (
     <div className="menu">
       <div className="menu-item" onClick={setActiveMenuCalendar} >
         Historical and future rates
       </div>
-      <div className="menu-item" onClick={() => setActiveMenu("My_history")}>
+      <div className="menu-item" onClick={setActiveMenuHistory}>
         My history
       </div>
       <div className="menu-item" onClick={handleCurrencyConversionClick}>
